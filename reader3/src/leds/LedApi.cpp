3e06c269ed0a81e055ea43f5785d9788f7379c29
ziposoft-ac <ac@ziposoft.com>
2026-07-18 18:52:03 -0400

    stuff

--- reader3/src/leds/LedApi.cpp
+++ reader3/src/leds/LedApi.cpp
@@ -3,10 +3,19 @@
 
 
 #define API(CLASS,OBJ,LIST) struct api##CLASS_t { LIST } api##CLASS;
-#define CMD(_X_) z_status call##_X_(_X_& data);
+#define CMD(_X_) z_status call##_X_(const _X_& data);
 
 
 LED_API
 
 #undef CMD
 #define CMD(X) { sizeof(X), #X,  }
+
+
+
+int foo()
+{
+
+
+    apiLedService.callLedFlash({LedGreen,1000,3});
+}
